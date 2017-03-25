1cd1e6106048843c47385486a45e93e9bf37f6e5
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-03-24 20:32:13 -0500

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+
+// topics array contains button text.
+
+var topics = ["The Gummy Bears", "David the Gnome", "Doug", "Teenage Mutant Ninja Turtles", "Darkwing Duck", "The Tick", "Spongebob", "She-ra", "The Fairly Odd Parents", "Jem and the Holograms", "Rocko's Modern Life", "Daria", "Recess", "Lloyd in Space", "Jimmy Neutron", "The Rocky and Bullwinkle Show", "Animaniacs", "Carebears", "Fragglerock", "My Little Pony"];
